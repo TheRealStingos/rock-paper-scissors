e2938e195a443a7a5271e55921ad0d561414d324
Justin Moore <Justmo10@gmail.com>
2023-02-24 11:53:31 -0500

    add basic game functionality

--- script.js
+++ script.js
@@ -6,4 +6,35 @@ function getComputerChoice(){
     return choice;
 }
 
-getComputerChoice(); 
+function checkWinner(playerSelection,computerSelection){
+    if (playerSelection == computerSelection){
+        return "Tie"
+    }
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")
+    ){
+        return "Player"
+    }
+    else {
+        return "Computer"
+    }
+}
+
+function playRound(playerSelection,computerSelection){
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result == "Tie"){
+        return "It's a Tie! No one wins!"
+    }
+    else if(result == "Player"){
+        return `Player wins! ${playerSelection} beats ${computerSelection}`
+    }
+    else{
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+const playerSelection = "rock"
+const computerSelection = getComputerChoice()
+console.log(playRound(playerSelection,computerSelection))
